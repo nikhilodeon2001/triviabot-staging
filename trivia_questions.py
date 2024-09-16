f2b2e934709822cf8f40d453a75b8fd034df4675
nikhilodeon2001 <35127542+nikhilodeon2001@users.noreply.github.com>
2024-09-15 21:47:55 -0700

    Update trivia_questions.py

--- trivia_questions.py
+++ trivia_questions.py
@@ -3,7 +3,7 @@
 trivia_questions = [
 ("What is the name for this type of image that can trick the brain into seeing a 3D scene?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/random/stereogram.jpgstereogram.jpg", ["Autostereogram", "Autostereograph"]), 
 ("What is the name for this type of image that can trick the brain into seeing a 3D scene?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/random/stereogram.jpgstereogram.jpg", ["Autostereogram", "Autostereograph"]),
-("What is the name for this type of image that can trick the brain into seeing a 3D scene?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/random/stereogram.jpgstereogram.jpg", ["Autostereogram", "Autostereograph"]),]  
+("What is the name for this type of image that can trick the brain into seeing a 3D scene?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/random/stereogram.jpgstereogram.jpg", ["Autostereogram", "Autostereograph"]),  
 ("What is the name for this type of image that can trick the brain into seeing a 3D scene?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/random/stereogram.jpgstereogram.jpg", ["Autostereogram", "Autostereograph"]),
 ("What is the name for this type of image that can trick the brain into seeing a 3D scene?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/random/stereogram.jpgstereogram.jpg", ["Autostereogram", "Autostereograph"]),
 ("What is the name for this type of image that can trick the brain into seeing a 3D scene?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/random/stereogram.jpgstereogram.jpg", ["Autostereogram", "Autostereograph"]),

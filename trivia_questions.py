4a6bb18eba2f581e3c4cb7c1a73f5244aee68ca9
nikhilodeon2001 <35127542+nikhilodeon2001@users.noreply.github.com>
2024-09-19 19:25:09 -0700

    Update trivia_questions.py

--- trivia_questions.py
+++ trivia_questions.py
@@ -1,15 +1,16 @@
 # trivia_questions.py
 
 trivia_questions = [
-("What is the name for this type of image that can trick the brain into seeing a 3D scene?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/random/stereogram.jpg", ["Autostereogram", "Autostereograph"]),
-("What is the name for this type of image that can trick the brain into seeing a 3D scene?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/random/stereogram.jpg", ["Autostereogram", "Autostereograph"]),
-("What is the name for this type of image that can trick the brain into seeing a 3D scene?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/random/stereogram.jpg", ["Autostereogram", "Autostereograph"]),
-("What is the name for this type of image that can trick the brain into seeing a 3D scene?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/random/stereogram.jpg", ["Autostereogram", "Autostereograph"]),
-("What is the name for this type of image that can trick the brain into seeing a 3D scene?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/random/stereogram.jpg", ["Autostereogram", "Autostereograph"]),
-("What is the name for this type of image that can trick the brain into seeing a 3D scene?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/random/stereogram.jpg", ["Autostereogram", "Autostereograph"]),
-("What is the name for this type of image that can trick the brain into seeing a 3D scene?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/random/stereogram.jpg", ["Autostereogram", "Autostereograph"]),
-("What is the name for this type of image that can trick the brain into seeing a 3D scene?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/random/stereogram.jpg", ["Autostereogram", "Autostereograph"]),
-("What is the name for this type of image that can trick the brain into seeing a 3D scene?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/random/stereogram.jpg", ["Autostereogram", "Autostereograph"]),
-("What is the name for this type of image that can trick the brain into seeing a 3D scene?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/random/stereogram.jpg", ["Autostereogram", "Autostereograph"]),
-("What is the name for this type of image that can trick the brain into seeing a 3D scene?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/random/stereogram.jpg", ["Autostereogram", "Autostereograph"]),
+("What is the name for this type of image that can trick the brain into seeing a 3D scene?","", ["Ohm's Law"]),
+("What is the name for this type of image that can trick the brain into seeing a 3D scene?","", ["Ohm's Law"]),
+("What is the name for this type of image that can trick the brain into seeing a 3D scene?","", ["Ohm's Law"]),
+("What is the name for this type of image that can trick the brain into seeing a 3D scene?","", ["Ohm's Law"]),
+("What is the name for this type of image that can trick the brain into seeing a 3D scene?","", ["Ohm's Law"]),
+("What is the name for this type of image that can trick the brain into seeing a 3D scene?","", ["Ohm's Law"]),
+("What is the name for this type of image that can trick the brain into seeing a 3D scene?","", ["Ohm's Law"]),
+("What is the name for this type of image that can trick the brain into seeing a 3D scene?","", ["Ohm's Law"]),
+("What is the name for this type of image that can trick the brain into seeing a 3D scene?","", ["Ohm's Law"]),
+("What is the name for this type of image that can trick the brain into seeing a 3D scene?","", ["Ohm's Law"]),
+("What is the name for this type of image that can trick the brain into seeing a 3D scene?","", ["Ohm's Law"]),
+("What is the name for this type of image that can trick the brain into seeing a 3D scene?","", ["Ohm's Law"])
 ]

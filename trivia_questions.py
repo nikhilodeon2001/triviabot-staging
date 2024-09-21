c10f17f8ce43c55c3b25128330bd6b5d6bb1c8b8
nikhilodeon2001 <35127542+nikhilodeon2001@users.noreply.github.com>
2024-09-21 14:02:13 -0700

    Update trivia_questions.py

--- trivia_questions.py
+++ trivia_questions.py
@@ -1,19 +1,18 @@
 # trivia_questions.py
 
 trivia_questions = [
-    ("What is the common name for this molecule?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/molecules/ozone.png", ["Ozone"]),
-    ("What is the common name for this molecule?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/molecules/methane.png", ["Methane"]),
-    ("What is the common name for this molecule?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/molecules/water.png", ["Water"]),
-    ("What is the common name for this molecule?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/molecules/ozone.png", ["Ozone"]),
-    ("What is the common name for this molecule?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/molecules/methane.png", ["Methane"]),
-    ("What is the common name for this molecule?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/molecules/water.png", ["Water"]),
-    ("What is the common name for this molecule?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/molecules/ozone.png", ["Ozone"]),
-    ("What is the common name for this molecule?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/molecules/methane.png", ["Methane"]),
-    ("What is the common name for this molecule?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/molecules/water.png", ["Water"]),
-    ("What is the common name for this molecule?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/molecules/ozone.png", ["Ozone"]),
-    ("What is the common name for this molecule?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/molecules/methane.png", ["Methane"]),
-    ("What is the common name for this molecule?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/molecules/water.png", ["Water"]),
-    ("What is the common name for this molecule?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/molecules/ozone.png", ["Ozone"]),
-    ("What is the common name for this molecule?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/molecules/methane.png", ["Methane"]),
-    ("What is the common name for this molecule?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/molecules/water.png", ["Water"]),
-]
+    ("The constellation pictured below was named after this hunter from the Odyssey.","https://triviabotwebsite.s3.us-east-2.amazonaws.com/astronomy/orion.png", ["Orion"]),
+    ("The constellations pictured below represent the 'Major' and 'Minor' forms of which constellation name?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/astronomy/ursa.jpg", ["Ursa"]),
+    ("What planet is pictured below?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/astronomy/jupiter.png", ["Jupiter"]),
+    ("What planet is pictured below?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/astronomy/mars.png", ["Mars"]),
+    ("What planet is pictured below?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/astronomy/neptune.png", ["Neptune"]),
+    ("What planet is pictured below?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/astronomy/saturn.png", ["Saturn"]),
+    ("What is the name of the  4,800 km wide gap between this planet's A and B Rings?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/astronomy/saturn.png", ["Cassini Division"]),
+    ("What is the occupation of the individual in the constellation pictured below?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/astronomy/orion.png", ["Hunter"]),
+    ("The constellations pictured below represent two of which type of animal?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/astronomy/ursa.jpg", ["Bear"]),
+    ("The planet below was named after a Roman god. What was his Greek name?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/astronomy/jupiter.png", ["Zeus"]),
+    ("The planet below was named after a Roman god. What was his Greek name?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/astronomy/mars.png", ["Ares"]),
+    ("The planet below was named after a Roman god. What was his Greek name?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/astronomy/neptune.png", ["Poseidon"]),
+    ("The planet below was named after a Roman god. What was his Greek name?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/astronomy/saturn.png", ["Cronus"]),
+    ("In the constellation below, on what clothing article can one find three bright and easily identifiable collinear star systems – Alnitak, Alnilam, and Mintaka","https://triviabotwebsite.s3.us-east-2.amazonaws.com/astronomy/orion.png", ["Belt"])
+    ]

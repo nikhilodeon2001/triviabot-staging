eb789ca483b10bce34d2eaf5df3de9c027198524
nikhilodeon2001 <35127542+nikhilodeon2001@users.noreply.github.com>
2024-09-23 13:29:40 -0700

    Update trivia_questions.py

--- trivia_questions.py
+++ trivia_questions.py
@@ -3,5 +3,6 @@
 trivia_questions = [
     ("What planet is pictured below?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/astronomy/mars.png", ["Mars"]),
     ("Unscramble this country:", "https://scramble4.com", ["Somalia"]),
-    ("Calculate the derivative with respect to x:", "https://polynomial1.com", ["Placeholder"])
+    ("Calculate the derivative with respect to x:", "https://polynomial1.com", ["Placeholder"]),
+    ("What is the capital of South Carolina?", "", ["Columbia"])
     ]

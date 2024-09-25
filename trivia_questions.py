5ed38b1496140478cd82bc00b32d5140fd49cfed
nikhilodeon2001 <35127542+nikhilodeon2001@users.noreply.github.com>
2024-09-25 16:33:57 -0700

    Update trivia_questions.py

--- trivia_questions.py
+++ trivia_questions.py
@@ -1,8 +1,17 @@
 # trivia_questions.py
 
 trivia_questions = [
-    ("What planet is pictured below?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/astronomy/mars.png", ["Mars"]),
-    ("Unscramble this country:", "https://scramble4.com", ["Somalia"]),
-    ("Calculate the derivative with respect to x:", "https://polynomial1.com", ["Placeholder"]),
-    ("What is the capital of South Carolina?", "", ["Columbia"])
-    ]
+# trivia_questions.py
+
+trivia_questions = [
+("Assume this is a regular polygon. What is θ?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/shapes/shapes-1-octagon-one.png", ["135"]),
+("Assume this is a regular polygon. What is θ (all angles)?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/shapes/shapes-10-triangle-all.png", ["180"]),
+("Assume this is a regular polygon. What is θ (all angles)?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/shapes/shapes-2-octagon-all.png", ["1080"]),
+("Assume this is a regular polygon. What is θ?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/shapes/shapes-3-hexagon-one.png", ["120"]),
+("Assume this is a regular polygon. What is θ (all angles)?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/shapes/shapes-4-hexagon-all.png", ["720"]),
+("Assume this is a regular polygon. What is θ?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/shapes/shapes-5-pentagon-one.png", ["108"]),
+("Assume this is a regular polygon. What is θ (all angles)?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/shapes/shapes-6-pentagon-all.png", ["540"]),
+("Assume this is a regular polygon. What is θ?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/shapes/shapes-7-square-one.png", ["90"]),
+("Assume this is a regular polygon. What is θ (all angles)?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/shapes/shapes-8-square-all.png", ["360"]),
+("Assume this is a regular polygon. What is θ?","https://triviabotwebsite.s3.us-east-2.amazonaws.com/shapes/shapes-9-triangle-one.png", ["60"])
+]  

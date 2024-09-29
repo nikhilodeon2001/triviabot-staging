3d6a192514249c9476e7f3a7d99adc8dc42b5ff6
nikhilodeon2001 <35127542+nikhilodeon2001@users.noreply.github.com>
2024-09-28 21:28:55 -0700

    Update triviabot.py

--- triviabot.py
+++ triviabot.py
@@ -1562,6 +1562,7 @@ def generate_and_render_polynomial_image(): #POLY
         print("Failed to upload the image to Matrix.")
 
 def round_preview(selected_questions):
+    print(selected_questions)
     message = "🔮 Next Round Preview 🔮\n"
     for i, question_data in enumerate(selected_questions, start=1):
         category = question_data[0]  # Category of the question

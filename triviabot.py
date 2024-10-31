b9a3e3d3c9f8ce9b6f0bc8dce9fb72b2809c2c52
nikhilodeon2001 <35127542+nikhilodeon2001@users.noreply.github.com>
2024-10-30 21:31:29 -0700

    Update triviabot.py

--- triviabot.py
+++ triviabot.py
@@ -142,7 +142,7 @@ def generate_magic_image(input_text):
         image_mxc = upload_image_to_matrix(image_data)
         image_size = 100
         
-        message = "🔍❓\n"
+        message = "🔍❓⬇\n"
         send_message(target_room_id, message)
     
         response = send_image(target_room_id, image_mxc, image_width, image_height, image_size)

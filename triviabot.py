13ec6d6d4330ebea0a0785e399eccdb2fe9f1bc0
nikhilodeon2001 <35127542+nikhilodeon2001@users.noreply.github.com>
2024-11-02 23:00:35 -0700

    Update triviabot.py

--- triviabot.py
+++ triviabot.py
@@ -1619,14 +1619,11 @@ def ask_question(trivia_category, trivia_question, trivia_url, trivia_answer_lis
         send_image_flag = True
 
     elif trivia_url == "multiple choice": 
-        message_body = f"\n{number_block} {get_category_title(trivia_category, trivia_url)}\n\n{trivia_question}\n"
+        message_body = f"\n{number_block} {get_category_title(trivia_category, trivia_url)}\n\n🚨 {trivia_question}\n"
         message_body += "\n"
         for answer in trivia_answer_list[1:]:
             message_body += f"{answer}\n"
-        
-        if trivia_answer_list[0] in {True, False}:
-            message_body += "\n🚨 1 Guess 🚨\n\n"
-        message_body += "\n🚨 Letter Only - 1 Guess 🚨\n\n"
+
 
         #image_mxc, image_width, image_height = generate_mc_image(trivia_answer_list)
         #image_size = 100

0f680eed32a3ad779989c991aa9add98584fc4cb
nikhilodeon2001 <35127542+nikhilodeon2001@users.noreply.github.com>
2024-11-06 10:05:13 -0800

    Update triviabot.py

--- triviabot.py
+++ triviabot.py
@@ -2566,7 +2566,7 @@ def update_round_streaks(user):
         send_message(target_room_id, message)
         time.sleep(2)
         
-        select_wf_quesitons()
+        select_wof_quesitons()
         
         gpt_summary = generate_round_summary(round_data, user)
 

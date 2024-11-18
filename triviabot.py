ce768000463839af141943cf74abfc506139c657
nikhilodeon2001 <35127542+nikhilodeon2001@users.noreply.github.com>
2024-11-18 13:56:34 -0800

    Update triviabot.py

--- triviabot.py
+++ triviabot.py
@@ -145,6 +145,7 @@ def fetch_new_donations():
         response = requests.get(url, headers=headers)
         if response.status_code == 200:
             data = response.json()
+            print("API Response Content:", response.content)
             new_donors = []
             
             for donor in data:

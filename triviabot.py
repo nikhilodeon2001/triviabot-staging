90680fa2aa7a338564fae3fbc380b8ff5aed20ea
nikhilodeon2001 <35127542+nikhilodeon2001@users.noreply.github.com>
2024-11-22 21:52:46 -0800

    Update triviabot.py

--- triviabot.py
+++ triviabot.py
@@ -2928,11 +2928,11 @@ def update_round_streaks(user):
         gpt_message = f"\n{gpt_summary}\n"
         send_message(target_room_id, gpt_message)
         
-        if current_longest_round_streak['streak'] > 1:
+        if current_longest_round_streak['streak'] > 2:
             generate_round_summary_image(round_data, user)
         else:
             time.sleep(4)
-            image_message = f"\n@{user} Get a win streak. Earn a custom image.\n"
+            image_message = f"\n@{user} Get a win streak over 2. Earn a custom image.\n"
             send_message(target_room_id, image_message)
             time.sleep(1)
 

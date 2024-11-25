e7356a81ac038028f8f8cdc5d563ca8d483caf1a
nikhilodeon2001 <35127542+nikhilodeon2001@users.noreply.github.com>
2024-11-24 19:50:05 -0800

    Update triviabot.py

--- triviabot.py
+++ triviabot.py
@@ -88,11 +88,11 @@ magic_time = 10
 magic_number = 0000
 
 
-num_mysterybox_clues_default = 1
+num_mysterybox_clues_default = 0
 num_mysterybox_clues = num_mysterybox_clues_default
-num_crossword_clues_default = 2
+num_crossword_clues_default = 0
 num_crossword_clues = num_crossword_clues_default
-num_jeopardy_clues_default = 2
+num_jeopardy_clues_default = 0
 num_jeopardy_clues = num_jeopardy_clues_default
 num_wof_clues_default = 0
 num_wof_clues = num_wof_clues_default

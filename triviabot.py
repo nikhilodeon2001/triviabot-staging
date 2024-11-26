a10e608ca38c85a8aa6f94610f7f0bd252348cf4
nikhilodeon2001 <35127542+nikhilodeon2001@users.noreply.github.com>
2024-11-25 19:57:55 -0800

    Update triviabot.py

--- triviabot.py
+++ triviabot.py
@@ -129,11 +129,10 @@ def upload_image_to_s3(image_url, winner):
         file_data = response.content
 
         # Step 2: Determine object name if not provided
-        if not object_name:
-            pst = pytz.timezone('America/Los_Angeles')
-            now = datetime.now(pst)
-            formatted_time = now.strftime('%B %d, %Y %H%M')  # Format: "November 25, 2024 1950"
-            object_name = f"{winner} ({formatted_time}).png"  # Add file extension if needed
+        pst = pytz.timezone('America/Los_Angeles')
+        now = datetime.now(pst)
+        formatted_time = now.strftime('%B %d, %Y %H%M')  # Format: "November 25, 2024 1950"
+        object_name = f"{winner} ({formatted_time}).png"  # Add file extension if needed
              
         # Step 3: Include the folder in the object name
         s3_key = f"{folder_name}/{object_name}"  # Add the folder name to the object path

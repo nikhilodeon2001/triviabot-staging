c7706822ca42ab352d897902eddbd438113af5e4
nikhilodeon2001 <35127542+nikhilodeon2001@users.noreply.github.com>
2025-01-31 20:51:41 -0800

    Update triviabot.py

--- triviabot.py
+++ triviabot.py
@@ -6187,7 +6187,7 @@ def update_round_streaks(user):
             if streak % discount_streak_amount == 0:
                 # Compute discount percentage
                 # e.g. if streak=10, discount_streak_amount=5, discount_step_amount=0.1 => 2 * 10% = 20%
-                discount_fraction = (streak // discount_streak_amount) * discount_step_amount
+                discount_fraction = min((streak // discount_streak_amount) * discount_step_amount, 90)
                 message += f"\n⚖️ Going forward @{user} will incur a -{discount_fraction}% handicap.\n"
                 
             message += f"\n▶️ Live trivia stats available: https://livetriviastats.com\n"
